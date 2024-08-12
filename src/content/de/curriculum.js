798341d4dd9e4075c291959ed063ec760e468ab2
Malte <malte.euler@gmail.com>
2024-08-12 23:59:03 +0200

    added matsunkuenpage and html akkordeon

---
+++ src/content/de/curriculum.js
@@ -0,0 +1,44 @@
+const grades = [
+    {
+        summary: "5º Student - yellow min. 3 month of practice",
+        content:"Standing Practice, Set of six Tsao (Exercises), The short form or shisan shi",
+    },
+    {
+        summary: "4º Student - orange min. 9 month of practice",
+        content:"Basic and Free Tui Shou Practice",
+    },
+    {
+        summary: "3º Student - green min. 18 month of practice",
+        content:"Movement Patterns: Kilometros, Rombo, Da Lu, and Saopapu.",
+    },
+    {
+        summary: "2º Student - blue min. 3 years of practice",
+        content:"The long form - A library for the techniques of Ma Tsun Kuen Tai Chi Chuan.",
+    },
+    {
+        summary: "1º Student - brown min. 5 years of practice",
+        content:"Daoist Breathing, Martial chi kung (I Ching Kung)",
+    },
+    {
+        summary: "1st Tuan  -  black min. 7 years of practice",
+        content:"Repeat all of the above on a higher level.",
+    },
+    {
+        summary: "2nd Tuan  -  black when master decides",
+        content:"The fast form",
+    },
+    {
+        summary: "3rd Tuan  -  black when master decides", 
+        content:"San Shou Tui Ta - The fighting form is a partner form examines martial aspects of Tai Chi Chuan.",
+    },
+    {
+        summary: "4rd Tuan  -  black when master decides",
+        content:"Hand/fist strikes, locks and pressures. Free fighting — friendly, of course",
+    },
+    {
+        summary: "5rd Tuan  -  black when master decides",
+        content:"Weapon Forms: Sword, Saber, Spear",
+    },
+]
+
+export default grades;
---
+++ src/content/en/curriculum.js
@@ -0,0 +1,64 @@
+const grades = [
+    {
+        summary: "5º Student, yellow — min. 3 month of practice",
+        content:"Standing Practice, Set of six Tsao (Exercises), The short form or shisan shi",
+        colors:"bg-yellow-300",
+        state: "open"
+    },
+    {
+        summary: "4º Student, orange — min. 9 month of practice",
+        content:"Basic and Free Tui Shou Practice",
+        colors:"bg-amber-400",
+        state: "closed"
+    },
+    {
+        summary: "3º Student, green — min. 18 month of practice",
+        content:"Movement Patterns: Kilometros, Rombo, Da Lu, and Saopapu.",
+        colors:"bg-green-500",
+        state: "closed"
+    },
+    {
+        summary: "2º Student, blue — min. 3 years of practice",
+        content:"The long form - A library for the techniques of Ma Tsun Kuen Tai Chi Chuan.",
+        colors:"bg-blue-500",
+        state: "closed"
+    },
+    {
+        summary: "1º Student, brown — min. 5 years of practice",
+        content:"Daoist Breathing, Martial chi kung (I Ching Kung)",
+        colors:"bg-amber-900 text-white",
+        state: "closed"
+    },
+    {
+        summary: "1st Tuan, black — min. 7 years of practice",
+        content:"Repeat all of the above on a higher level.",
+        colors:"bg-stone-800 text-white",
+        state: "closed"
+    },
+    {
+        summary: "2nd Tuan, black — when master decides",
+        content:"The fast form",
+        colors:"bg-stone-800 text-white",
+        state: "closed"
+    },
+    {
+        summary: "3rd Tuan, black — when master decides", 
+        content: "**San Shou Tui Ta**<br/>The fighting form is a partner form. It examines martial aspects of Tai Chi Chuan.",
+        colors:"bg-stone-800 text-white",
+        state: "closed"
+    },
+    {
+        summary: "4rd Tuan, black — when master decides",
+        content:"Hand/fist strikes, locks and pressures. Free fighting — friendly, of course",
+        colors:"bg-stone-800 text-white",
+        state: "closed"
+    },
+    {
+        summary: "5rd Tuan, black — when master decides",
+        content:"Weapon Forms: Sword, Saber, Spear",
+        colors:"bg-stone-800 text-white",
+        state: "closed"
+    },
+]
+
+export default grades;
