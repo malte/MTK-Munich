2c098d4488bad7727043b30a1beba67f6ba7b009
Malte <malte.euler@gmail.com>
2024-08-15 17:31:24 +0200

    typographic things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   //  outDir: './MTK-Munich-dist',
   //  output: 'static',
+  site: 'https://xn--taichi-mnchen-3ob.art/',
   integrations: [tailwind({
     nesting: true
-  }), mdx(), alpinejs()]
+  }), mdx(), alpinejs(), sitemap()]
 });
